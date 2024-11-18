373b33203944e11b390ae6c6809d50c3dde6510c
tronin1000 <tigoventuratec@gmail.com>
2024-11-18 19:35:55 -0300

    conectar com aixos

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,75 +1,123 @@
-import { FlatList, Text, TextInput, View, StyleSheet, Button, TouchableOpacity} from "react-native"
-import { useState } from "react"
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-
-
-export const HomeScreen = () =>{
-    const [tarefa, setTarefa] = useState ("")
-
+import {
+    FlatList,
+    Text,
+    TextInput,
+    View,
+    StyleSheet,
+    Button,
+    TouchableOpacity,
+  } from "react-native";
+  import { useState } from "react";
+  import { tarefa } from "../../types/types";
+  import FontAwesome from "@expo/vector-icons/FontAwesome";
+  
+  export const HomeScreen = () => {
+    const [tarefa, setTarefa] = useState("");
     const [listaTarefas, setListaTarefas] = useState<any[]>([]);
-
-    const handlePress = () => {
-       if (tarefa == "") return;
-        const novaTarefa = {
-            title: tarefa
-        };
-
-        setListaTarefas([...listaTarefas, novaTarefa]);
-
-        setTarefa("");
-    }
-
+  
+    const adicionarTarefa = () => {
+      if (tarefa == "") return;
+      // const novaTarefa = {
+      //   id: "",
+      //   titulo: "",
+      //   descricao: "",
+      //   status: false
+      // }
+      const novaTarefa = {
+        titulo: tarefa,
+      };
+  
+      setListaTarefas([...listaTarefas, novaTarefa]);
+  
+      setTarefa("");
+    };
+  
+    const deletarTarefa = (itemIndex: number) => {
+      console.log("Deletar Tarefa. Index: ", itemIndex);
+      const listaFiltrada = listaTarefas.filter(
+        (item, index) => index !== itemIndex
+      );
+      setListaTarefas(listaFiltrada);
+    };
+  
+    //Texto para um lado e ícone para o outro
+    //Criem uma função para excluir um item do array
+    //Utilizem o filter no listaTarefas
+  
     return (
-        <View style={style.container}>
-            <View style={style.containerInput}>
-            <TextInput style={style.input} value={tarefa} onChangeText={setTarefa}/>
-            <Button title="Adicionar Tarefa" onPress={handlePress}/>
-            </View>
-
-            <FlatList 
-            data={listaTarefas}
-            renderItem={({item, index}) => (
-                <View style={style.itemContainer}>
-                <Text>{item.title}</Text>
-                <TouchableOpacity>
-                <FontAwesome name="trash-o" size={50} color="black" />
-                </TouchableOpacity>
-                
-                </View>
-            )}
-            keyExtractor={(item, index) => index.toString()}
-            />
+      <View style={styles.container}>
+        {/* A entrada de tarefas */}
+        <View style={styles.containerInput}>
+          <TextInput
+            style={styles.input}
+            value={tarefa}
+            onChangeText={setTarefa}
+          />
+          <Button title="Adicionar Tarefa" onPress={adicionarTarefa} />
         </View>
-    )
-} 
-
-const style = StyleSheet.create({
-    container:{ 
-        backgroundColor:"#0ff",
-        flex:1,
-        padding:16,
-        
+        {/* Exibição das tarefas */}
+        <FlatList
+          style={styles.lista}
+          data={listaTarefas}
+          renderItem={({ item, index }) => (
+            <View style={styles.itemContainer}>
+              <Text style={styles.itemText} numberOfLines={1}>
+                {item.titulo}
+              </Text>
+              <View style={styles.iconContainer}>
+                <TouchableOpacity>
+                  <FontAwesome name="pencil" size={24} color="white" />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => deletarTarefa(index)}>
+                  <FontAwesome name="trash-o" size={24} color="white" />
+                </TouchableOpacity>
+              </View>
+            </View>
+          )}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </View>
+    );
+  };
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 16,
+      // alignItems: "center",
+      // backgroundColor: "gray",
     },
     containerInput: {
- 
+      // alignItems: "center",
     },
-
-    input:{
-        borderWidth:1,
-        borderBlockColor:"#ccc",
-        borderRadius:8,
-        backgroundColor: "white",
-        alignItems:"center"
-
+    input: {
+      borderWidth: 1,
+      borderColor: "#ccc",
+      borderRadius: 8,
+      backgroundColor: "#fff",
+      // width: "85%",
+      // alignSelf: "center",
+      marginBottom: 6,
     },
-
-    itemContainer:
-    {
-        //flexDirection:"row",
-        justifyContent:"space-between",
-        backgroundColor:"red",
-            
-    }
-
-    
-});
+    lista: {
+      marginTop: 8,
+    },
+    itemContainer: {
+      flexDirection: "row",
+      backgroundColor: "steelblue",
+      justifyContent: "space-between",
+      padding: 16,
+      marginBottom: 8,
+      borderRadius: 8,
+    },
+    itemText: {
+      fontWeight: 500,
+      fontSize: 18,
+      color: "#fff",
+    },
+    iconContainer: {
+      flexDirection: "row",
+      gap: 10,
+    },
+  });
+  
